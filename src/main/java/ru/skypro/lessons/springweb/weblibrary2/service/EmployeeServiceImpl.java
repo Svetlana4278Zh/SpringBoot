c5c734d68305c04f363ca87e0e2e00d854b18b11
Svetlana4278Zh <svetlana.zh@inbox.ru>
2023-06-18 22:55:07 +0300

    Homework 07.06

--- src/main/java/ru/skypro/lessons/springweb/weblibrary2/service/EmployeeServiceImpl.java
+++ src/main/java/ru/skypro/lessons/springweb/weblibrary2/service/EmployeeServiceImpl.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
@@ -36,120 +38,167 @@ public class EmployeeServiceImpl implements EmployeeService{
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final ReportRepository reportRepository;
 
+    Logger logger = LoggerFactory.getLogger(EmployeeServiceImpl.class);
+
     public List<EmployeeDTO> getAllEmployees() {
-        return employeeRepository.findAllEmployees()
+        logger.info("Was invoked method for get a list of employees");
+        List<EmployeeDTO> employeeDTOList = employeeRepository.findAllEmployees()
                 .stream()
                 .map(EmployeeDTO::fromEmployee)
                 .collect(Collectors.toList());
+        logger.debug("Successful access to the database");
+        return employeeDTOList;
     }
 
     @Override
     public void addEmployee(Employee employee) {
+        logger.info("Was invoked method for add a employee");
         employeeRepository.save(employee);
+        logger.debug("Successful access to the database");
     }
 
     @Override
     public void editEmployee(int id, Employee employee) {
+        logger.info("Was invoked method for edit a employee with id = {}",id);
         Employee employeeEdit = employeeRepository.findById(id);
         employeeEdit.setName(employee.getName());
         employeeEdit.setSalary(employee.getSalary());
         employeeEdit.setPosition(employee.getPosition());
         employeeRepository.save(employeeEdit);
+        logger.debug("Successful access to the database");
     }
 
     @Override
     public EmployeeDTO getEmployeeById(int id) {
-        return fromEmployee(employeeRepository.findById(id));
+        logger.info("Was invoked method for get a employee with id = {}",id);
+        EmployeeDTO employeeDTO = fromEmployee(employeeRepository.findById(id));
+        logger.debug("Successful access to the database");
+        return employeeDTO;
     }
 
     @Override
     public void deleteEmployeeById(int id) {
+        logger.info("Was invoked method for delete a employee with id = {}",id);
         employeeRepository.deleteById(id);
+        logger.debug("Successful access to the database");
     }
 
     @Override
     public List<EmployeeDTO> getEmployeesWithSalaryHigherThan(int salary) {
-        return getAllEmployees().stream()
+        logger.info("Was invoked method for get a list of employees with salary higher than {}",salary);
+        List<EmployeeDTO> employeeDTOList = getAllEmployees().stream()
                 .filter(employee -> employee.getSalary() > salary)
                 .collect(Collectors.toList());
+        logger.debug("Successful access to the database");
+        return employeeDTOList;
     }
 
     @Override
     public EmployeeDTO getEmployeeWithMaxSalary() {
-        return getAllEmployees().stream()
+        logger.info("Was invoked method for get a employee with max salary");
+        EmployeeDTO employeeDTO = getAllEmployees().stream()
                 .max((o1, o2) -> o1.getSalary()-o2.getSalary())
                 .get();
+        logger.debug("Successful access to the database");
+        return employeeDTO;
     }
 
     @Override
     public List<EmployeeDTO> getEmployeeswithHighestSalary() {
+        logger.info("Was invoked method for get a list of employees with highest salary");
         int maxSalary = getEmployeeWithMaxSalary().getSalary();
-        return getAllEmployees().stream()
+        List<EmployeeDTO> employeeDTOList = getAllEmployees().stream()
                 .filter(employee -> employee.getSalary() == maxSalary)
                 .collect(Collectors.toList());
+        logger.debug("Successful access to the database");
+        return employeeDTOList;
     }
 
     @Override
     public List<EmployeeDTO> getEmployeeswithPosition(String position) {
+        logger.info("Was invoked method for get a list of employees with position {}",position);
         try {
             position.isEmpty();
-            return employeeRepository.findAllEmployees().stream()
+            List<EmployeeDTO> employeeDTOList = employeeRepository.findAllEmployees().stream()
                     .filter(employee -> employee.getPosition().getNamePosition().equals(position))
                     .map(EmployeeDTO::fromEmployee)
                     .collect(Collectors.toList());
-        } catch (NullPointerException e){return getAllEmployees();}
+            logger.debug("Successful access to the database");
+            return employeeDTOList;
+        } catch (NullPointerException e){
+            logger.error(e.getMessage(),e);
+            return getAllEmployees();
+        }
     }
 
     @Override
     public List<EmployeeFullInfo> findAllEmployeeFullInfo() {
-        return employeeRepository.findAllEmployeeFullInfo();
+        logger.info("Was invoked method for get a list of employees, full info");
+        List<EmployeeFullInfo> employeeFullInfoList = employeeRepository.findAllEmployeeFullInfo();
+        logger.debug("Successful access to the database");
+        return employeeFullInfoList;
     }
 
     @Override
     public EmployeeFullInfo getEmployeeByIdFullInfo(int id) {
-        return employeeRepository.findByIdFullInfo(id);
+        logger.info("Was invoked method for get a employee with id = {}, full info", id);
+        EmployeeFullInfo employeeFullInfo = employeeRepository.findByIdFullInfo(id);
+        logger.debug("Successful access to the database");
+        return employeeFullInfo;
     }
 
     @Override
     public List<EmployeeDTO> getEmployeeWithPaging(Integer pageIndex, Integer unitPerPage) {
+        logger.info("Was invoked method for get a list of employees, page {}", pageIndex);
         if (pageIndex == null){
             pageIndex = 0;
         }
         Pageable employeeOfConcretePage = PageRequest.of(pageIndex, unitPerPage);
         Page<Employee> page = employeeRepository.findAll(employeeOfConcretePage);
 
-        return page.stream()
+        List<EmployeeDTO> employeeDTOList = page.stream()
                 .map(EmployeeDTO::fromEmployee)
                 .toList();
+        logger.debug("Successful access to the database");
+        return employeeDTOList;
     }
 
     public void addEmployeeFromFile(MultipartFile file){
+        logger.info("Was invoked method for add employees from file \"{}\"", file.getOriginalFilename());
         File fileEmployee = new File(file.getOriginalFilename());
         try {
             List<Employee> employeeList = objectMapper.readValue(fileEmployee, new TypeReference<>(){});
             for (Employee value : employeeList) {
                 employeeRepository.save(value);
             }
+            logger.debug("Successful access to the database");
         } catch (IOException e) {
+            logger.error(e.getMessage(), e);
             e.printStackTrace();
         }
     }
     public int report(){
+        logger.info("Was invoked method for save the report and get its id");
         try{
             Report report = new Report();
             report.setReport(buildReport());
-            return reportRepository.save(report).getId();
+            int reportId = reportRepository.save(report).getId();
+            logger.debug("Successful access to the database");
+            return reportId;
         } catch (JsonProcessingException e){
+            logger.error(e.getMessage(), e);
             e.printStackTrace();
             throw new InternalServerError();
         }
     }
     public String buildReport() throws JsonProcessingException {
+        logger.info("Was invoked method for generating the report");
         List<ReportDTO> reports = employeeRepository.buildReportsDTO();
         return objectMapper.writeValueAsString(reports);
     }
 
     public Resource downloadReport(int id){
+        logger.info("Was invoked method for download the report");
         return new ByteArrayResource(reportRepository.findById(id)
                 .orElseThrow(ReportNotFoundException::new)
                 .getReport()
